08e4250bc70eeb43a11d3860bd663716d7de9389
WarpPrism <249662605@qq.com>
2015-03-12 14:26:55 +0800

    完成了JavaScript

--- Lab2表格排序/table-sorter/sorter.js
+++ Lab2表格排序/table-sorter/sorter.js
@@ -1,26 +1,102 @@
-window.onload = function () {
+window.onload = function() {
 	var tables = getAllTables();
-	//alert(tables[0].id);
 	makeAllTablesSortable(tables);
 }
+
 function getAllTables() {
 	var tables = document.getElementsByTagName("table");
 	return tables;
 }
-function makeAllTablesSortable(element) {
-	for (var i = 0; i < element.length; i++) {
-		var ths = element[i].getElementsByTagName("th");
-		var length = ths.length;
-		for (var j = 0; j < length; j++) {
-			ths[j].onclick = function() {
-				var thead = document.getElementsByTagName("th");
-				for (var k = 0; k < thead.length; k++) {
-					if (thead[k] != this) {
-						thead[k].style.background = "#000080";
+
+function makeAllTablesSortable(tables) {
+	for (var i = 0; i < tables.length; i++) {
+		var ths = tables[i].getElementsByTagName("th");
+		theadClick(ths.length, ths, tables[i]);
+	}
+}
+
+function theadClick(length, ths, table) {
+	for (var j = 0; j < length; j++) {
+		ths[j].onclick = function(j, length, ths, table) {
+			return function() {
+				if (this.className == "descend" || this.className == "") {
+					for (var k = 0; k < length; k++) {
+						if (k != j) {
+							ths[k].className = "";
+						}
 					}
+					this.className = "ascend";
+					ascend(j, table);
+				} else if (this.className == "ascend") {
+					for (var k = 0; k < length; k++) {
+						if (k != j) {
+							ths[k].className = "";
+						}
+					}
+					this.className = "descend";
+					descend(j, table);
 				}
-				this.style.background = "url('./ascend.png') no-repeat right #a4b1fb";
+			}
+		}(j, length, ths, table);
+	}
+}
+
+function ascend(curcol, table) {
+	var body = table.getElementsByTagName("tbody");
+	var bodyrow = body[0].getElementsByTagName("tr");
+	var cells = new Array();
+	for (var i = 0; i < bodyrow.length; i++) {
+		var tds = bodyrow[i].getElementsByTagName("td");
+		cells[i] = tds[curcol];
+		//alert(cells[i].innerHTML);
+	}
+	for (var i = 0; i < cells.length; i++) {
+		var min = cells[i].innerHTML;
+		var index = i;
+		var temp = 0;
+		for (var j = i; j < cells.length; j++) {
+			if (cells[j].innerHTML < min) {
+				min = cells[j].innerHTML;
+				index = j;
 			}
 		}
+		temp = cells[i];
+		cells[i] = cells[index];
+		cells[index] = temp;
+		swapRow(i + 1, index + 1, table);
 	}
+}
+
+function descend(curcol, table) {
+	var body = table.getElementsByTagName("tbody");
+	var bodyrow = body[0].getElementsByTagName("tr");
+	var cells = new Array();
+	for (var i = 0; i < bodyrow.length; i++) {
+		var tds = bodyrow[i].getElementsByTagName("td");
+		cells[i] = tds[curcol];
+		//alert(cells[i].innerHTML);
+	}
+	for (var i = 0; i < cells.length; i++) {
+		var max = cells[i].innerHTML;
+		var index = i;
+		var temp = 0;
+		for (var j = i; j < cells.length; j++) {
+			if (cells[j].innerHTML > max) {
+				max = cells[j].innerHTML;
+				index = j;
+			}
+		}
+		temp = cells[i];
+		cells[i] = cells[index];
+		cells[index] = temp;
+		swapRow(i + 1, index + 1, table);
+	}
+}
+
+function swapRow(a, b, table) {
+	var rows = table.getElementsByTagName("tr");
+	var temp;
+	temp = rows[a].innerHTML;
+	rows[a].innerHTML = rows[b].innerHTML;
+	rows[b].innerHTML = temp;
 }
